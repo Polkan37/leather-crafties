52f2335724ebcf864c886a574c3428c1658a3e79
Polkan37 <zaq2587413699@gmail.com>
2022-06-17 15:41:35 +0300

    header + footer

---
+++ components/burger.js
@@ -0,0 +1,20 @@
+// burger menu header
+
+const iconBurger = document.querySelector(".navigation-burger");
+const navigationsBody = document.querySelector(".navigation-wrapper");
+iconBurger.addEventListener("click", () => {
+  document.body.classList.toggle("lock");
+  iconBurger.classList.toggle("active");
+  navigationsBody.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".navigation__link");
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    if (iconBurger.classList.contains("active")) {
+      document.body.classList.remove("lock");
+      iconBurger.classList.remove("active");
+      navigationsBody.classList.remove("active");
+    }
+  });
+});
