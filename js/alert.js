4b1459a6dad74666f23ac8cf49c27b47d0976d1f
Margarita Neskoromna <u3-025@office121.local>
2023-07-11 15:00:51 +0300

    fix: formating

--- js/alert.js
+++ js/alert.js
@@ -1,10 +1,9 @@
 function showAlert(message, success = true) {
-    document.querySelector(".alert-container").innerHTML = `<div class="alert ${
-      success ? "alert-success" : "alert-danger"
+  document.querySelector(".alert-container").innerHTML = `<div class="alert ${success ? "alert-success" : "alert-danger"
     } alert-dismissible fade show" role="alert">
             <strong>${message}</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
        </div>`;
-    const bsAlert = new bootstrap.Alert(document.querySelector(".alert"));
-    setTimeout(() => bsAlert.close(), 2000);
-  }
+  const bsAlert = new bootstrap.Alert(document.querySelector(".alert"));
+  setTimeout(() => bsAlert.close(), 2000);
+}
